287633feeb8376074332b07558dd0d5a9c345fff
Leonardo Fabían Hernández Peña <ingenieroleonardo@outlook.com>
2021-02-11 10:57:41 -0500

    Cambios de imagen y aplicación de filtros

--- src/assets/scripts/frontend.js
+++ src/assets/scripts/frontend.js
@@ -1,10 +1,22 @@
 import url from 'url';
 import path from 'path';
+import applyFilter from './filters';
+import { setIpc, sendIpc } from './ipcRendererEvents';
 
 window.addEventListener('load', () => {
+    setIpc();
     GetImagesEvent();
     searchImages();
+    selectFilter();
+    OpenDirectory();
 });
+
+function OpenDirectory() {
+    const openDirectory = document.getElementById('open-directory');
+    openDirectory.addEventListener('click', () => {
+        sendIpc()
+    })
+}
 
 function GetImagesEvent() {
     const thumbs = document.querySelectorAll('li.list-group-item');
@@ -16,35 +28,46 @@ function GetImagesEvent() {
     }
 }
 
+function selectFilter() {
+    const select = document.getElementById('filters');
+
+    select.addEventListener('change', function () {
+        applyFilter(this.value, document.getElementById('image-displayed'));
+    })
+}
+
 function changeImage(node) {
-    document.querySelector('li.selected').classList.remove('selected');
-    node.classList.add('selected');
-    document.getElementById('image-displayed').src = node.querySelector('img').src;
+    if (node) {
+        document.querySelector('li.selected').classList.remove('selected');
+        node.classList.add('selected');
+        document.getElementById('image-displayed').src = node.querySelector('img').src;
+    } else {
+        document.getElementById('image-displayed').src = '';
+    }
 }
 
 function searchImages() {
     const searchBox = document.getElementById('search-box');
 
     searchBox.addEventListener('keyup', function () {
-        if (this.value.length > 0) {
-            const regex = new RegExp(this.value.toLowerCase(), 'gi');
-            if (this.value.length > 0) {
-                const thumbs = document.querySelectorAll('li.list-group-item img');
-                for (let index = 0; index < thumbs.length; index++) {
-                    const fileUrl = url.pathToFileURL(thumbs[index].src);
-                    const fileName = path.basename(fileUrl.pathname);
-                    console.log(fileName);
-                    if (fileName.match(regex)) {
-                        thumbs[index].parentNode.classList.remove('hidden');
-                    } else {
-                        thumbs[index].parentNode.classList.remove('hidden');
-                    }
-                }
+        const regex = new RegExp(this.value.toLowerCase(), 'gi');
 
-                selectFirstImage();
+        if (this.value.length > 0) {
+            const thumbs = document.querySelectorAll('li.list-group-item img');
+            for (let index = 0; index < thumbs.length; index++) {
+                const fileUrl = url.pathToFileURL(thumbs[index].src);
+                const fileName = path.basename(fileUrl.pathname);
+                if (fileName.match(regex)) {
+                    thumbs[index].parentNode.classList.remove('hidden');
+                } else {
+                    thumbs[index].parentNode.classList.add('hidden');
+                }
             }
+
+            selectFirstImage();
         } else {
             showAll();
+            selectFirstImage();
         }
     });
 }
@@ -55,10 +78,9 @@ function selectFirstImage() {
 }
 
 function showAll() {
-    const listItems = document.querySelectorAll('.li.list-group-item');
+    const thumbs = document.querySelectorAll('li.list-group-item img');
 
-    for (let index = 0; index < listItems.length; index++) {
-        listItem[index].classList.remove('hidden');
+    for (let index = 0; index < thumbs.length; index++) {
+        thumbs[index].parentNode.classList.remove('hidden');
     }
-    selectFirstImage();
 }
